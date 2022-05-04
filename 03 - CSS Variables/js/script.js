44d076fb89a41dbe138e0aa7aea926628b701da1
Галюзин Виталий Юрьевич <v.galyuzin@lugacom.local>
2022-05-04 08:23:25 +0300

    i analyzed the code and added another editable property for the third project

--- 03 - CSS Variables/js/script.js
+++ 03 - CSS Variables/js/script.js
@@ -1,10 +1,12 @@
 
-const inputs = document.querySelectorAll('.controls input');
+const inputs = document.querySelectorAll('.controls input'); // Создаём коллекцию из всех control инпутов
 
-function handleUpdate() {
-  const suffix = this.dataset.sizing || '';
-  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+function handleUpdate() { // Обновление 
+  const suffix = this.dataset.sizing || ''; // dataset предоставляет доступ к записи пользовательских атрибутов (data-*)
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix); // Задаём свойство для стилей коренного элемента документа (html)
 }
 
-inputs.forEach(input => input.addEventListener('change', handleUpdate));
+// this.name - необходимый инпут, this.value - его значение, которое мы редактируем через суффикс
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate)); // Добавляем слушатели при наведении и изменении списка инпутов
 inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
