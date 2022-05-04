1a4f00c642ffa1a1b90daa351a6ac9de015de90d
Галюзин Виталий Юрьевич <v.galyuzin@lugacom.local>
2022-05-04 10:06:53 +0300

    finalized the third application, added another property

--- 03 - CSS Variables/js/script.js
+++ 03 - CSS Variables/js/script.js
@@ -2,11 +2,15 @@
 const inputs = document.querySelectorAll('.controls input'); // Создаём коллекцию из всех control инпутов
 
 function handleUpdate() { // Обновление 
-  const suffix = this.dataset.sizing || ''; // dataset предоставляет доступ к записи пользовательских атрибутов (data-*)
-  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix); // Задаём свойство для стилей коренного элемента документа (html)
-}
+  const suffix = this.dataset.sizing || ''; // dataset предоставляет доступ к просмотру пользовательских атрибутов (data-*)
 
-// this.name - необходимый инпут, this.value - его значение, которое мы редактируем через суффикс
+  // suffix равен указанному в инпуте атрибуту dataset-sizing или ничему в случае если атрибут отсутствует 
+
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix); // Задаём свойство для стилей коренного элемента документа (html)
+} 
+
+// переписывается root, в котором находятся css-переменные
+// this.name - имя редактируемого инпута, this.value - его значение + суффикс
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate)); // Добавляем слушатели при наведении и изменении списка инпутов
 inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
